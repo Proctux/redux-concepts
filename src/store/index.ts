71a5d693a0fa5e128da6848335c9452aaef0d1d3
Luiz <luiz.ado@hotmail.com>
2020-12-10 09:43:54 -0300

    Added a new component called CatalogItem to abstract some information into a new component.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import rootReducer from '../store/modules/rootReducer';
 
 import { ICartState } from '../store/modules/cart/types';
 
-const store = createStore(rootReducer);
-
 export interface IState {
   cart: ICartState
 }
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
