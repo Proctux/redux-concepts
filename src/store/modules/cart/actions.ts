9b99f524880bf98838e1d689824582e03b36b93a
Luiz <luiz.ado@hotmail.com>
2020-12-10 12:15:54 -0300

    Added 3 different types of actions for add product to cart.

--- src/store/modules/cart/actions.ts
+++ src/store/modules/cart/actions.ts
@@ -1,8 +1,26 @@
 import { IProduct } from './types';
 
-export const addProductToCart = (product: IProduct) => {
+export const addProductToCartRequest = (product: IProduct) => {
   return {
-    type: 'ADD_PRODUCT_TO_CART',
+    type: 'ADD_PRODUCT_TO_CART_REQUEST',
+    payload: {
+      product
+    }
+  }
+}
+
+export const addProductToCartSuccess = (productId: number) => {
+  return {
+    type: 'AAD_PRODUCT_TO_CART_SUCCESS',
+    payload: {
+      productId
+    }
+  }
+}
+
+export const addProductToCartFailure = (product: IProduct) => {
+  return {
+    type: 'ADD_PRODUCT_TO_CART_FAILURE',
     payload: {
       product
     }
