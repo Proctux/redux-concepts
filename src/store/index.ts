4cd8575c417a0ee875725d2379c2b774509b415b
Luiz <luiz.ado@hotmail.com>
2020-12-09 23:04:42 -0300

    Added Redux types.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,12 @@
 import { createStore } from 'redux';
 import rootReducer from '../store/modules/rootReducer';
 
+import { ICartState } from '../store/modules/cart/types';
+
 const store = createStore(rootReducer);
+
+export interface IState {
+  cart: ICartState
+}
 
 export default store;
